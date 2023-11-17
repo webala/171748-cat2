3a73f98d4646c2f97ce683d7823102f25b21dca4
webala <webdspam@gmail.com>
2023-11-17 09:05:19 +0300

    added comments

--- Q3.cpp
+++ Q3.cpp
@@ -2,6 +2,8 @@
 #include <string>
 using namespace std;
 
+
+//This function dispays the menu
 void showMenu() {
     cout << "\n\nChoose an option" << endl;
    cout << "1. Go to About us" << endl;
@@ -12,8 +14,11 @@ void showMenu() {
 
 int main() {
 
+   //Set the username and password
   string username = "username";
   string password = "password";
+
+  //Define the inputs
   string usernameInput, passwordInput;
    cout << "Enter the username " << endl;
    cin >> usernameInput;
@@ -21,13 +26,17 @@ int main() {
    cin >> passwordInput;
    bool loggedIn = false;
 
+//Check if username and passwor is correct, if correct, log in the user
    if (username == usernameInput && password == passwordInput) {
       loggedIn = true;
+
+      // Continue looping until user selects 4 to log out
       while (loggedIn) {
          showMenu();
          int userSelection;
          cin >> userSelection;
 
+         //Use a switch case to display the various pages
          switch (userSelection)
          {
          case 1:
